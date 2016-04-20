628245cea7bf0efe0f15b4481398b716cfc69519
Adam Lyons <adam@tanda.co>
2016-04-20 11:17:12 +1000

    UPDATE: comments and example use case

--- python/api_methods.py
+++ python/api_methods.py
@@ -1,7 +1,13 @@
-### Methods for API using Python
-
+'''
+Python methods for Tanda API.  You will need to install Requests: HTTP for Humans
+pip install requests
+http://docs.python-requests.org/en/master/
+'''
 import requests
 
+# Creates new token based on Tandau sername, password and scope
+# You can view your tokens here https://my.tanda.co/api/oauth/access_tokens
+# View available scopes https://my.tanda.co/api/v2/documentation#header-scopes
 def authenticate(username, password, scope = 'me'):
   url = 'https://my.tanda.co/api/oauth/token'
   body = {'username': username, 'password': password, 'scope': scope, 'grant_type': 'password'}
@@ -36,5 +42,12 @@ def delete(extension, token):
   requests.delete(base_url + extension, headers=headers)
 
 
-token = authenticate(USERNAME, PASSWORD)
-print token
+#Get a Token which you will use to authenticate yourself
+#Seperate scopes with spaces or leave blank for default scope
+token = authenticate(USERNAME,PASSWORD,"user me")
+
+#Use token to get information about your user.
+user = get("users/me",token)
+
+#Print the content of the request i.e. user details
+print user.content
